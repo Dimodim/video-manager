a5181ae1b0882b2847cbbaa45aef641ea660bb6c
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-03-12 19:27:24 +0200

    added vuelidate

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
@@ -12,6 +13,7 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    'prefer-destructuring': ['error', { object: true, array: false }],
     'import/no-named-as-default': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
